57ddbe098211b321f9e23015fe37c1c0b7daa6be
Gabriel-otirB <lordglayder@gmail.com>
2025-03-28 10:56:37 -0300

    Created question structure

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request as FacadesRequest;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
@@ -15,7 +17,69 @@ class MainController extends Controller
         $this->app_data = require(app_path('app_data.php'));
     }
 
-    public function showData() {
-        return response()->json($this->app_data);
+    public function startGame(): View
+    {
+        return view('home');
+    }
+
+    public function prepareGame(Request $request)
+    {
+        // validate request
+        $request->validate(
+            [
+                'total_questions' => ['required', 'integer', 'min:3', 'max:30']
+            ],
+            [
+                'total_questions.required' => 'O número de questões é obrigatório',
+                'total_questions.integer' => 'O número de questões tem que ser um valor inteiro',
+                'total_questions.min' => 'Selecione no mínimo :min questões',
+                'total_questions.max' => 'Selecione no máximo :max questões',
+            ]
+        );
+
+        // get total questions
+        $total_questions = intval($request->input('total_questions'));
+
+        // prepare all the quiz structure
+        $quiz = $this->prepareQuiz($total_questions);
+
+        dd($quiz);
+    }
+
+    private function prepareQuiz($total_questions)
+    {
+        $questions = [];
+        $total_countries = count($this->app_data);
+
+        // create countries index for unique questions
+        $indexes = range(0, $total_countries - 1);
+        shuffle($indexes);
+        $indexes = array_slice($indexes, 0, $total_questions);
+
+        // create array of questions
+        $question_number = 1;
+        foreach ($indexes as $index) {
+
+            $question['question_number'] = $question_number++;
+            $question['country'] = $this->app_data[$index]['country'];
+            $question['correct_answer'] = $this->app_data[$index]['capital'];
+
+            // wrong answers
+            $other_capitals = array_column($this->app_data, 'capital');
+
+            // remove correct answer
+            $other_capitals = array_diff($other_capitals, [$question['correct_answer']]);
+
+            // shuffle the wrong answers
+            shuffle($other_capitals);
+            $question['wrong_answers'] = array_slice($other_capitals, 0, 3);
+
+            // store answer result
+            $question['correct'] = null;
+
+            $questions[] = $question;
+        }
+
+        return $questions;
     }
 }
